1b75de93b729912d9bd3286f046beedaba091b4b
Volkan <56156700+VolkanGoksu@users.noreply.github.com>
2020-09-09 00:18:01 +0300

    uygulama

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      count:0
+    };
+  }
+  raiseCount(){
+     this.setState({
+       count:this.state.count +1
+     });
+  };
+  dropCount(){
+    this.setState({
+      count:this.state.count -1
+    });
+  };
+  render(){
+    return (
+      <div className="App">
+          <div class="ui label">
+           <i class="mail icon"></i> {this.state.count}
+          </div>
+         <div>
+         <button className="positive ui button"onClick={()=>this.raiseCount()} >+</button>
+         <button className="positive ui button"onClick={()=>this.dropCount()} >-</button>
+         </div>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
